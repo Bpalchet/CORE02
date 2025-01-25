5636d66d9288ab8271b36167da34aac6a8900b86
Bpalchet <bernie@palchetti.me>
2025-01-25 17:29:31 +0100

    Add files via upload

---
+++ EXAM02/ft_putstr/ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpalchet <bpalchet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 16:27:13 by bpalchet          #+#    #+#             */
+/*   Updated: 2025/01/25 16:37:51 by bpalchet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+	write (1, "\n", 1);
+	return ;
+}
+
+/*int	main(void)
+{
+	char *str = "Hello 42 Firenze!";
+
+	ft_putstr(str);
+	return (0);
+}*/
